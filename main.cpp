f61c45f7bf64aad4f2612af39c7ab853015a8e02
FrancisBFTC <bftcorporations@gmail.com>
2022-09-02 14:15:20 -0300

    Insere funcao main() vazia
    * Comecaremos o desenvolvimento do codigo principal para chamar as outras funcoes

--- main.cpp
+++ main.cpp
@@ -3,4 +3,14 @@
     Data    : 02/09/2022
     Programa: Calculadora simples em C++ para CMD
 
+    @branch main
+    @commit Insere funcao main() vazia
+    @description Comecaremos o desenvolvimento do codigo principal para chamar as outras funcoes
 */
+
+int main(void){
+
+    // aqui vai o nosso codigo
+    
+    return 0;    
+}
