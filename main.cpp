3fc9dcee6f02c099dc69a9ead51bac8076815ffc
FrancisBFTC <bftcorporations@gmail.com>
2022-09-02 19:27:39 -0300

    Commit automatico do GitDocker 1
    Este commit nao foi efetuado pelo usuario.

--- main.cpp
+++ main.cpp
@@ -8,10 +8,51 @@
     @description Comecaremos o desenvolvimento do codigo principal para chamar as outras funcoes
 */
 
-int main(void){
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
 
-    // aqui vai o nosso codigo
-    
+int Add_Num(int, int);
+int Sub_Num(int, int);
+int Mul_Num(int, int);
+int Div_Num(int, int);
+
+int main(void){
+    int op, num1, num2, res;
+    char rep = 's';
+
+    while(rep == 's'){
+        system("ECHO OFF");
+        system("CLS");
+        cout << "Escolha uma operacao:" << endl << endl;
+        cout << "1. Adicao" << endl;
+        cout << "2. Subtracao" << endl;
+        cout << "3. Multiplicacao" << endl;
+        cout << "4. Divisao" << endl;
+        cin >> op;
+
+        if(op >= 1 && op <= 4){
+            cout << endl;
+            cout << "Primeiro Numero: ";
+            cin >> num1;
+            cout << "Segundo Numero: ";
+            cin >> num2;
+            switch(op){
+                case 1: res = Add_Num(num1, num2); break;
+                case 2: res = Sub_Num(num1, num2); break;
+                case 3: res = Mul_Num(num1, num2); break;
+                case 4: res = Div_Num(num1, num2); break;
+                default: break;
+            }
+
+            cout << endl << "O resultado e : " << res << endl;
+        }else{
+            cout << "Operacao Invalida!" << endl;
+        }
+        cout << "Tentar novamente? (s/n)";
+        cin >> rep;
+    }
+
     return 0;    
 }
 
@@ -50,3 +91,4 @@ int Mul_Num(int x, int y){
 int Div_Num(int x, int y){
     return x / y;
 }
+
